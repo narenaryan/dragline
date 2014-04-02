a92aff5e8535113f4e33b764c5199f764710bfb1
Ashwin Rajeev <ashwin@quadloops.com>
2014-04-02 16:33:32 +0530

    create basic ds required to crawl

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import httplib2
 import httpcache2
+from redisds import RedisQueue, RedisSet
 import sys
 import os
 
+if len(sys.argv) > 1:
+    sys.path.insert(0, sys.argv[1])
+    import main
+    print main.START_URLS
+else:
+    exit()
+
 http = httplib2.Http()
+url_queue = RedisQueue(main.NAME, 'urls')
+visited_urls = RedisSet(main.NAME, 'visited')
 
 
 def process_url(url):
     head, content = http.request(url, 'GET')
     print head
-
-
-
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        sys.path.insert(0, sys.argv[1])
-        import main
-        print main.START_URL
