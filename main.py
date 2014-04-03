2e83624a7685506d1e9c03158ef982f9f8030910
Ashwin Rajeev <ashwin@quadloops.com>
2014-04-03 11:38:49 +0530

    parse urls from content and insert to queue

--- main.py
+++ main.py
@@ -1,44 +1,80 @@
+from gevent import monkey, spawn, joinall
+monkey.patch_all()
+
 import httplib2
 import httpcache2
 from redisds import RedisQueue, RedisSet
-from gevent import monkey, spawn
+from gevent.coros import BoundedSemaphore
+from urlparse import urljoin, urldefrag
+from lxml import html
 import sys
 import os
+import re
 
-monkey.patch_all()
 
 class Crawl(object):
     lock = BoundedSemaphore(1)
+    current_urls = set()
     running_count = 0
+
     def __init__(self, module):
-        self.url_queue = RedisQueue(module.name, 'urls')
-        self.visited_urls = RedisSet(module.name, 'visited')
+        self.url_queue = RedisQueue(module.NAME, 'urls')
+        self.visited_urls = RedisSet(module.NAME, 'visited')
+        self.url_pattern = re.compile(module.ALLOWED_URLS[0])
         for i in module.START_URLS:
-            self.url_queue.put(i)
-        http = httplib2.Http()
+            self.insert(i)
+        self.http = httplib2.Http()
+        self.parsers = module.PARSERS
 
     def count(self):
-        return running_count
+        return Crawl.running_count
 
-    def inc_coubt(self):
+    def inc_count(self, url):
         Crawl.lock.acquire()
-        Crawl.running_count +=1
+        Crawl.current_urls.add(url)
+        Crawl.running_count += 1
         Crawl.lock.release()
 
-    def dec_coubt(self):
+    def dec_count(self, url):
         Crawl.lock.acquire()
-        Crawl.running_count -=1
+        Crawl.current_urls.remove(url)
+        Crawl.running_count -= 1
         Crawl.lock.release()
+
+    def insert(self, url):
+        if not any(url in i for i in (Crawl.current_urls, self.visited_urls, self.url_queue)):
+            self.url_queue.put(url)
+
+    def parse(self, baseurl, content):
+        data = html.fromstring(content)
+        for url in data.xpath('//a/@href'):
+            url = urldefrag(urljoin(baseurl, url))[0]
+            if self.url_pattern.match(url):
+                self.insert(url)
 
     def process_url(self):
-        self.url_queue.get(timeout=10)
-        head, content = self.http.request(url, 'GET')
+        while True:
+            url = self.url_queue.get(timeout=2)
+            print url
+            if url:
+                self.inc_count(url)
+                head, content = self.http.request(url, 'GET')
+                self.parse(url, content)
+                self.visited_urls.add(url)
+                self.dec_count(url)
+            else:
+                if not self.count():
+                    break
 
 
 if len(sys.argv) > 1:
     sys.path.insert(0, sys.argv[1])
     import main
-    crawler = Crawl(main)
-    spawn(crawler.process_url())
+    crawlers = []
+    for i in xrange(5):
+        crawler = Crawl(main)
+        crawlers.append(spawn(crawler.process_url))
+    joinall(crawlers)
+    print "finished"
 else:
     exit()
