1fe338eb85ba3295fab4e35b3083eb9d08abd044
shimil <shimil@inzyte.com>
2014-04-03 14:56:53 +0530

    url parser added

---
+++ parser.py
@@ -0,0 +1,14 @@
+import re
+from html import lxml
+
+class Parser:
+	def __init__(self,allowed_urls):
+		self.url_pattern=re.compile('(%s)'%'|'.join(re.escape(allowed_urls)))
+
+	def parse(self,baseurl,content):
+		data = html.fromstring(content)
+        for url in data.xpath('//a/@href'):
+            url = urldefrag(urljoin(baseurl, url.strip()))[0]
+            if self.url_pattern.match(url):
+                self.insert(re.escape(url))
+
