13e6951bf3ffe63292965ed5ba8c5431e9765008
Ashwin Rajeev <ashwin@quadloops.com>
2014-04-03 09:25:28 +0530

    basic crawler layout

--- main.py
+++ main.py
@@ -1,21 +1,44 @@
 import httplib2
 import httpcache2
 from redisds import RedisQueue, RedisSet
+from gevent import monkey, spawn
 import sys
 import os
+
+monkey.patch_all()
+
+class Crawl(object):
+    lock = BoundedSemaphore(1)
+    running_count = 0
+    def __init__(self, module):
+        self.url_queue = RedisQueue(module.name, 'urls')
+        self.visited_urls = RedisSet(module.name, 'visited')
+        for i in module.START_URLS:
+            self.url_queue.put(i)
+        http = httplib2.Http()
+
+    def count(self):
+        return running_count
+
+    def inc_coubt(self):
+        Crawl.lock.acquire()
+        Crawl.running_count +=1
+        Crawl.lock.release()
+
+    def dec_coubt(self):
+        Crawl.lock.acquire()
+        Crawl.running_count -=1
+        Crawl.lock.release()
+
+    def process_url(self):
+        self.url_queue.get(timeout=10)
+        head, content = self.http.request(url, 'GET')
+
 
 if len(sys.argv) > 1:
     sys.path.insert(0, sys.argv[1])
     import main
-    print main.START_URLS
+    crawler = Crawl(main)
+    spawn(crawler.process_url())
 else:
     exit()
-
-http = httplib2.Http()
-url_queue = RedisQueue(main.NAME, 'urls')
-visited_urls = RedisSet(main.NAME, 'visited')
-
-
-def process_url(url):
-    head, content = http.request(url, 'GET')
-    print head
