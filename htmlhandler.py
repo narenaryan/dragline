1e1bd4e7baee54d7277bdf9b55fb7e782fdecc84
shimil <shimil@inzyte.com>
2014-04-04 14:25:34 +0530

    bugs fixed

--- htmlhandler.py
+++ htmlhandler.py
@@ -1,5 +1,5 @@
 import re
-from lxml import html
+
 
 from urlparse import urljoin, urldefrag
 
@@ -14,23 +14,16 @@ class Parser:
     def parse(self, head, baseurl, content):
         #print baseurl
         if "text/html" in head['content-type']:
-            data = html.fromstring(content)
+            
 
             for parser in self.parsers:
-                self.process(baseurl, parser, data)
+                
+                if re.match(re.escape(parser.__regex__),baseurl):
+                    parser.__process__(baseurl,content)
 
             for url in data.xpath('//a/@href'):
                 url = urldefrag(urljoin(baseurl, url.strip()))[0]
                 if self.url_pattern.match(url):
                     yield url
 
-    def process(self, baseurl,parser, data):
-        
-        if not re.match(re.escape(parser.__regex__),baseurl):
-            return
-        
-
-        item = dict()
-        for title, xpath in parser.__xpath__.items():
-            item[title] = data.xpath(xpath)
-        parser.__process__(item)
+    
