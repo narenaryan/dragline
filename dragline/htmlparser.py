76229d4c6610363e42e8c1edcd51c880d95609f4
shimil <shimil@inzyte.com>
2014-05-20 12:57:17 +0530

    added htmlparser

---
+++ dragline/htmlparser.py
@@ -0,0 +1,21 @@
+from lxml import etree
+from urlparse import urldefrag, urljoin
+
+class HtmlParser:
+    def __init__(self, url, content):
+        self.url = url
+        self.data=etree.HTML(content)
+
+
+    def extract_urls(self,xpath):
+        return (self.abs_url(self.url, url) for url in self.data.xpath(xpath + '//a/@href'))
+
+
+    def extract(self,xpath):
+        return self.data.xpath(xpaths)
+
+
+
+    def abs_url(self, baseurl, relativeurl):
+        return urldefrag(urljoin(baseurl, relativeurl.strip()))[0]
+
