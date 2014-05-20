9ef75c20930b4581a2130337bdfb95acd8ac57a8
Ashwin Rajeev <ashwin@quadloops.com>
2014-05-20 17:09:13 +0530

    change default request content-type

--- dragline/defaultsettings.py
+++ dragline/defaultsettings.py
@@ -1,4 +1,5 @@
 REQUEST_HEADERS = {
     "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36",
-    "accept": "text/html"
+    "accept": "text/html",
+    'content-type': "application/x-www-form-urlencoded"
 }
