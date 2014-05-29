9f4ce76bb9c3f228dd9c57d4134aaf2af9ea3c48
shimil <shimil@inzyte.com>
2014-05-29 17:39:34 +0530

    generate.py bug fixing

--- dragline/generate.py
+++ dragline/generate.py
@@ -2,10 +2,10 @@ from string import Template
 import os
 import argparse
 def generate(spider_name):
-    main = open("templates/main.py","r").read()
+    main = open(os.path.dirname(os.path.abspath(__file__))+"/templates/main.py","r").read()
     s = Template(main)
     main = s.substitute(spider_name=spider_name)
-    settings = open("templates/settings.py","r").read()
+    settings = open(os.path.dirname(os.path.abspath(__file__))+"/templates/settings.py","r").read()
 
     os.makedirs(spider_name)
     mainfile = open(spider_name+"/main.py","w")
