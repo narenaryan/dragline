6d73acf073a49611ac561d5618780ecb7f6d4ed6
shimil <shimil@inzyte.com>
2014-05-30 10:31:56 +0530

    generate.py bug fixing

--- dragline/htmlparser.py
+++ dragline/htmlparser.py
@@ -8,11 +8,36 @@ class HtmlParser:
         self.url = response.url
         self.data = etree.HTML(response.body)
 
-    def extract_urls(self, xpath):
+    def extract_urls(self, xpath=""):
+
+        """This function extracts urls from a given xpath
+
+        Kwargs:
+       xpath (str): xpath from which the urls are to be fetched
+
+
+
+
+    Returns:
+       :list of urls.
+
+
+
+
+    >>> html = HtmlParser(response)
+    >>> urls = html.extract_urls("EXAMPLE_XPATH")
+    ['url1',['url2'],['url3']
+
+
+    """
         return [self.abs_url(self.url, url)
                 for url in self.data.xpath(xpath + '//a/@href')]
 
     def xpath(self, xpath):
+        """This function extracts contents of an xpath
+
+
+        """
         return self.data.xpath(xpath)
 
     def abs_url(self, baseurl, relativeurl):
