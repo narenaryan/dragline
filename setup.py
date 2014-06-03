572b09bf87db6a4e16baadae75a1e695b0d06d66
Ashwin Rajeev <ashwin@quadloops.com>
2014-06-03 10:25:26 +0530

    add licence

--- setup.py
+++ setup.py
@@ -5,17 +5,27 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='Dragline',
-      version='0.8',
+      version=open('dragline/VERSION').read().strip(),
       description='Spider framework',
       author='Ashwin Rajeev, Shimil Rahman',
       author_email='ashwin@inzyte.com, shimil@inzyte.com',
-      maintainer='Ashwin Rajeev',
-      maintainer_email='ashwin@inzyte.com',
       url='http://www.inzyte.com',
       packages=['dragline'],
       scripts=glob("scripts/*"),
       data_files=[('templates', glob("templates/*"))],
       include_package_data=True,
-      install_requires=[i.strip() for i in open('requirements.txt').xreadlines()],
-      test_suite='tests'
+      install_requires=['gevent>=1.0.0', 'httplib2>=0.9', 'redis>=2.9.0'],
+      test_suite='tests',
+      classifiers=[
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: POSIX :: Linux',
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Environment :: Console',
+          'Topic :: Software Development :: Libraries :: Application Frameworks',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: Internet :: WWW/HTTP',
+      ]
       )
