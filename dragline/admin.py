4d9163b132c20e2062dba7a9504bf3eee3ec495a
Ashwin Rajeev <ashwin@quadloops.com>
2014-06-27 17:56:42 +0530

    cleanup dragline-admin

--- dragline/admin.py
+++ dragline/admin.py
@@ -9,8 +9,10 @@ import base64
 from runner import load_modules
 import argparse
 import zipfile
+import pkgutil
 
 config_file = os.path.expanduser('~/.dragline')
+
 
 def zipdir(source, destination):
     folder = os.path.abspath(source)
@@ -68,7 +70,7 @@ def upload(url, username, password, foldername, spider_website=None):
     return content
 
 
-def deploy(serv_name,spider_name):
+def deploy(serv_name, spider_name):
     parser = ConfigParser.SafeConfigParser()
     parser.read(config_file)
     args = dict(parser.items(serv_name))
@@ -76,13 +78,11 @@ def deploy(serv_name,spider_name):
     print upload(**args)
 
 
-
 def generate(spider_name):
-    template_dir = os.path.join(dragline.__path__[0], "templates")
-    main = open(os.path.join(template_dir, "main.tem"), "r").read()
+    main = pkgutil.get_data(__package__, "templates/main.tem")
     s = Template(main)
     main = s.substitute(spider_name=spider_name)
-    settings = open(os.path.join(template_dir, "settings.tem"), "r").read()
+    settings = pkgutil.get_data(__package__, "templates/settings.tem")
 
     os.makedirs(spider_name)
     mainfile = open(spider_name + "/main.py", "w")
@@ -92,10 +92,11 @@ def generate(spider_name):
     settfile.write(settings)
     settfile.close()
 
+
 def add_server(serv_name):
-    url=raw_input("Enter URL:")
-    usr_name=raw_input("Enter Username:")
-    pwd=raw_input("Enter the password:")
+    url = raw_input("Enter URL:")
+    usr_name = raw_input("Enter Username:")
+    pwd = raw_input("Enter the password:")
     parser = ConfigParser.SafeConfigParser()
     parser.read(config_file)
     if not parser.has_section(serv_name):
@@ -103,32 +104,33 @@ def add_server(serv_name):
     parser.set(serv_name, 'url', url)
     parser.set(serv_name, 'username', usr_name)
     parser.set(serv_name, 'password', pwd)
-    parser.write(open(config_file,'w'))
+    parser.write(open(config_file, 'w'))
 
 
 def execute():
     parser = argparse.ArgumentParser(description='Dragline commandparse')
-    subparsers = parser.add_subparsers(title='subcommands', description='valid subcommands',help='additional help')
+    subparsers = parser.add_subparsers(
+        title='subcommands', description='valid subcommands', help='additional help')
 
     parser_first = subparsers.add_parser('init')
     parser_first.set_defaults(which='init')
-    parser_first.add_argument('spider',help='spider name')
+    parser_first.add_argument('spider', help='spider name')
 
     parser_second = subparsers.add_parser('deploy')
     parser_second.set_defaults(which='deploy')
-    parser_second.add_argument('server',help='server name')
-    parser_second.add_argument('spider',help='spider name')
+    parser_second.add_argument('server', help='server name')
+    parser_second.add_argument('spider', help='spider name')
 
-    parser_third= subparsers.add_parser('addserver')
+    parser_third = subparsers.add_parser('addserver')
     parser_third.set_defaults(which='addserver')
-    parser_third.add_argument('server',help='assign work for a server')
-    
+    parser_third.add_argument('server', help='assign work for a server')
+
     args = vars(parser.parse_args())
 
     if args['which'] == 'init':
         generate(args['spider'])
     elif args['which'] == 'deploy':
-        deploy(args['server'],args['spider'])
+        deploy(args['server'], args['spider'])
     elif args['which'] == 'addserver':
         add_server(args['server'])
 
