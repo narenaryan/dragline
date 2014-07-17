86288d6d076b05e5cbd2db93f53991ec10c81dac
shimil <shimil@inzyte.com>
2014-07-17 17:24:36 +0530

    Added connection in headers, crawling getting stucked bug fixed

--- dragline/defaultsettings.py
+++ dragline/defaultsettings.py
@@ -12,7 +12,8 @@ class RequestSettings(Settings):
     HEADERS = {
         "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36",
         "accept": "text/html",
-        'content-type': "application/x-www-form-urlencoded"
+        'content-type': "application/x-www-form-urlencoded",
+        'Connection': 'close'
     }
     AUTOTHROTTLE = False
     CACHE = None
